199e13758a151044eb94d0989a0b395617ac2092
Justice <justicejacobs2@gmail.com>
2021-01-08 20:01:17 -0500

    Update RecurringValue.cpp

--- RecurringValue.cpp
+++ RecurringValue.cpp
@@ -1,21 +1,27 @@
+/*
+* 
+CLI Program to count the number of times a recurring value appears
+
+*/
+
 #include <iostream>
 // using namespace std;
 
 int main() {
-	int currVal = 0, val = 0;
 
-	// read first number, test data validity
+	int currVal = 0, val = 0;
 	if (std::cin >> currVal) { // if able to read and store in variable
 		int cnt = 1; // store current value for input in cnt 
 		while (std::cin >> val) { // read remaining numbers
-			if (val == currVal)  // if current value = previous value, add one 
-				++cnt;
+			if (val == currVal)  // if current value = previous value
+				++cnt; // add one
 
 			else {
+
 				std::cout << currVal << " occurs "
 					<< cnt << "number of times" << std::endl;
-				currVal = val;
-				cnt = 1;
+				currVal = val; // reset current value 
+				cnt = 1; // reset count
 			}
 		}
 		std::cout << currVal << "occurs " << cnt << "times" << std::endl;
